4b05f5950d7e61cf7b05e0bcc6f45981d2028d00
Lex <Zo6pusha@gmail.com>
2018-01-11 01:07:24 -0600

    Delete camel.py

--- camel.py
+++
@@ -1,9 +0,0 @@
-words = []
-keys = input('Enter Words \n')
-user_words = keys.split()
-#[item.lower() for item in user_words]
-words.append(keys.lower())
-for i in words:
-    j = i.replace(' ','')
-#print("".join(words))
-print(j)
